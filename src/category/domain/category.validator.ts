77f863ec3abf41b9192e9114dec46793e47e67a9
Thiago-Cardoso <thiago.cardosoo@hotmail.com>
2024-05-06 07:47:33 -0300

    refactor: adjustment in validation package

--- src/category/domain/category.validator.ts
+++ src/category/domain/category.validator.ts
@@ -1,14 +1,34 @@
-import { IsString, MaxLength  } from "class-validator";
+import { IsString, MaxLength, IsOptional, IsBoolean, IsNotEmpty   } from "class-validator";
+import { Category } from "./category.entity";
+import { ClassValidatorFields } from "../../shared/domain/validators/class-validator-fields";
 
-class CategoryRules {
+export class CategoryRules {
   @MaxLength(255)
   @IsString()
   @IsNotEmpty()
   name: string;
+
+  @IsString()
+  @IsOptional()
   description: string | null;
+
+  @IsBoolean()
+  @IsNotEmpty()
   is_active: boolean;
+
+  constructor({ name, description , is_active}: Category){
+    Object.assign(this, { name, description, is_active})
+  }
 }
 
-class CategoryValidator {
+export class CategoryValidator extends ClassValidatorFields<CategoryRules> {
+  validate(entity: Category) {
+    return super.validate(new CategoryRules(entity));
+  }
+}
 
+export class CategoryValidatorFactory {
+  static create() {
+    return new CategoryValidator();
+  }
 }
